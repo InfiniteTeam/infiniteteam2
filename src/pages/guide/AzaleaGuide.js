2a481dff5241e1a0e15126eb3335054568cc2631
ArpaAP <qndus1705@naver.com>
2020-08-10 05:57:53 +0000

    markdown styling

--- src/pages/guide/AzaleaGuide.js
+++ src/pages/guide/AzaleaGuide.js
@@ -5,15 +5,20 @@ import { Container } from 'react-bootstrap'
 
 export default class App extends Component {
   render() {
-    return (
-      <>
-        <Container fluid="sm" style={{
-          marginTop: 100,
-          marginBottom: 100
-        }}>
-          <ReactMarkdown source={getting_started}/>
-        </Container>
-      </>
-    );
+	return (
+	  <>
+		<Container fluid="sm" style={{
+		  marginTop: 100,
+		  marginBottom: 100
+		}}>
+		  <ReactMarkdown
+			source={getting_started}
+			transformImageUri={
+				uri => `${process.env.REACT_IMAGE_BASE_URL}${uri}`	
+			  }
+		  />
+		</Container>
+	  </>
+	);
   }
 }
