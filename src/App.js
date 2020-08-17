572b1477f2b2cd33f6e8ecd1f53edbf7725fa714
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-08-17 19:17:04 +0900

    개인정보처리방침 작성중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navibar, Footer } from './components'
 import { Route, Switch } from 'react-router-dom';
-import { Home, NotFound } from './pages';
+import { Home, NotFound, Privacy } from './pages';
 import { Azalea } from './pages/bots';
 import { Guide } from './pages/guide';
 import guides from './pages/guide/guides';
@@ -14,8 +14,9 @@ export default class App extends Component {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/bots/azalea" component={Azalea}/>
-          <Route exact path="/guide/:whose" render={props => <Guide whose={props.match.params.whose} name={guides.filter(one => one.home)[0].name} />} />
+          <Route exact path="/guide/:whose" render={props => <Guide whose={props.match.params.whose} name={guides.filter(one => one.home && props.match.params.whose === one.whose)[0].name} />} />
           <Route exact path="/guide/:whose/:id" render={props => <Guide whose={props.match.params.whose} name={props.match.params.id} />} />
+          <Route exact path="/privacy" component={Privacy}/>
           <Route component={NotFound}/>
         </Switch>
         <Footer />
