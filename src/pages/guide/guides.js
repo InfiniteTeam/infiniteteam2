cfd4a61553b2b1621fea5e6b4f8b126a2db23fac
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-09-22 22:59:38 +0900

    Add/Mod: 가이드 개선 및 아즈트라 가이드 작성중

--- src/pages/guide/guides.js
+++ src/pages/guide/guides.js
@@ -1,25 +1,33 @@
-import { getting_started, make_char, manage_char } from './source';
+import * as azalea from './source/azalea';
+import * as aztra_tips from './source/aztra_tips'
 
 const guides = [
   {
     name: "getting_started",
     title: "시작하기",
-    src: getting_started,
+    src: azalea.getting_started,
     whose: "azalea",
     home: true
   },
   {
     name: "make_char",
     title: "캐릭터 생성",
-    src: make_char,
+    src: azalea.make_char,
     whose: "azalea",
     home: false
   },
   {
     name: "manage_char",
     title: "캐릭터 관리",
-    src: manage_char,
+    src: azalea.manage_char,
     whose: "azalea",
+    home: false
+  },
+  {
+    name: "template_formatting",
+    title: "메시지 템플릿",
+    src: aztra_tips.template_formatting,
+    whose: "aztra_tips",
     home: false
   }
 ];
